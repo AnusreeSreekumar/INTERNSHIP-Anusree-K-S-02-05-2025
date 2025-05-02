0f9fc8299fb408645a526a16fd96705671f7b37b
Anusree Sreekumar <anusree.ks@gmail.com>
2025-05-02 21:57:18 +0530

    commit complete

--- Models/studentSchema.js
+++ Models/studentSchema.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
 const studentSchema = new mongoose.Schema({
-    dbname: {type: String, required: true},
-    dbrollnum: {type: Number, default: 1, unique: true, required: true},
-    dbmobile: {type: Number, required: true},
-    dbclassId: {type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true}
+  dbname: { type: String, required: true },
+  dbrollnum: { type: Number, required: true },
+  dbmobile: { type: Number, required: true },
+  dbclassId: { type: mongoose.Schema.Types.ObjectId, ref: "Class", required: true },
 });
 
+studentSchema.index({ dbrollnum: 1, dbclassId: 1 }, { unique: true });
+
 const Student = mongoose.model("Student", studentSchema);
+
 export default Student;
